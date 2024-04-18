c90608c82889f9bdedfe756ac15302eb8b7ca0c6
Sandra Siliņa <sandra.silina@lumii.lv>
2024-04-18 13:08:50 +0300

    Added script for easier source data download

--- connectedComponents/README.md
+++ connectedComponents/README.md
@@ -2,11 +2,16 @@
 This directory contains all pre-processing scripts and programs for [ComponentVis](https://github.com/IMCS-Bioinformatics/ChromatinNetworkVisualisation/tree/main/Visualizations/ComponentVisualization).
 
 [process-hic](./process-hic) includes code for transforming Hi-C data from its original format to a format that is used for other programs and adding ChromHMM annotations, genes and proteins.
+
 Code for finding connected components is in [Find_components](./Find_components).
+
 [Python_programs](./Python_programs) contains code for calculating additional data about the connected components and dataset in general, as well as combining multiple files for the same chromosome into ZIP files.
 
 ## Usage
 Clone this directory to your local computer to use it.
+
+downloadSourceData.py contains a script that creates the intended directories and downloads input data from our server.
+
 Included bash scripts allow to more easily run all programs sequentially. The bash scripts should be run from this directory.
 
 ### Dependencies
---
+++ connectedComponents/downloadSourceData.py
@@ -0,0 +1,45 @@
+import urllib.request
+import os
+
+replace_existing_files=False
+
+def download_file(url, local_path):
+    if not replace_existing_files and os.path.exists(local_path):
+        raise Exception(f"Local file already exists: {os.getcwd()}/{local_path}")
+    else:
+        print(f"Downloading file: {url}", flush=True)
+        urllib.request.urlretrieve(url, local_path)
+
+os.chdir(os.path.dirname(__file__))
+
+data_url = "http://susurs.mii.lu.lv/HiCData/"
+roadmap_ids = ["E080", "E065", "E066", "E096", "E097", "E098", "E100", "E109", "E113"]
+hic_tissue_names_ids = {"Adrenal_gland": "AD", "Aorta": "AO", "Bladder": "BL", "Liver": "LI", "Lung": "LG", "Ovary": "OV", "Pancreas": "PA", "Psoas": "PO", "Small_Bowel": "SB", "Spleen": "SX"}
+pchic_tissue_ids = ["AD2", "AO", "BL1", "LG", "LI11", "OV2", "PA", "PO3", "SB", "SX"]
+
+state_path = "Source_data/chromatin_states/"
+state_path_bed = f"{state_path}18states.all.mnemonics.bedFiles/"
+os.makedirs(state_path_bed, exist_ok=True)
+normal_hic_path = "Source_data/Normal_HiC(hg19,3DIV_legacy)_10/"
+os.makedirs(normal_hic_path, exist_ok=True)
+pchic_path = "Source_data/pcHi-C(hg19)/"
+os.makedirs(pchic_path, exist_ok=True)
+
+for id in roadmap_ids:
+    download_file(f"{data_url}18states.all.mnemonics.bedFiles/{id}_18_core_K27ac_mnemonics.bed.gz", f"{state_path_bed}{id}.bed.gz")
+
+download_file(f"{data_url}Tissue_name_mapping/roadmap-normalHiC.csv", f"{state_path}roadmap-normalHiC.csv")
+download_file(f"{data_url}Tissue_name_mapping/roadmap-pcHiC.csv", f"{state_path}roadmap-pcHiC.csv")
+
+for name, id in hic_tissue_names_ids.items():
+    os.makedirs(f"{normal_hic_path}{name}", exist_ok=True)
+    download_file(f"{data_url}Normal_HiC(hg19,3DIV_legacy)_selection/{name}/3DIV_{id}_cutoff-10.zip", f"{normal_hic_path}{name}/3DIV_{id}_cutoff-10.zip")
+    download_file(f"{data_url}Normal_HiC(hg19,3DIV_legacy)_selection/{name}/3DIV_{id}_cutoff-2.zip", f"{normal_hic_path}{name}/3DIV_{id}_cutoff-2.zip")
+
+for id in pchic_tissue_ids:
+    download_file(f"{data_url}pcHi-C(hg19)/{id}.po.txt.zip", f"{pchic_path}{id}.po.txt.zip")
+    download_file(f"{data_url}pcHi-C(hg19)/{id}.pp.txt.zip", f"{pchic_path}{id}.pp.txt.zip")
+
+download_file(f"{data_url}Tissue_name_mapping/tissue-id-name-roadmap.csv", f"{pchic_path}tissue-id-name-roadmap.csv")
+
+download_file(f"{data_url}hg19_genes_symbols.csv", "Source_data/hg19_genes+symbols.csv")
